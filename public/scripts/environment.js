9b9ed6d3f59de5998b70245ab5aff5da763016a5
navisantos <sou.ivan@gmail.com>
2019-09-04 08:51:38 -0300

    medals

--- public/scripts/environment.js
+++ public/scripts/environment.js
@@ -16,25 +16,25 @@ var productCollection = "ofertas";
 
 /*SEGUNDO PASSO: Criar um atributo Custom chamado NBO e colocar o API Name dele*/
 /*NBO*/
-var NBO = "NBO_c";
+var NBO = "NBO";
 
 
 /*IDs das Medalhas*/
-var prod_1_1 = "BadgePrimeirosPassos_c";
-var prod_1_2 = "BadgeConvideAmigo_c";
+var prod_1_1 = "BadgePrimeirosPassos";
+var prod_1_2 = "BadgeConvideAmigo";
 var prod_1_3 = "collaboration";
 var prod_2_1 = "congrats";
 var prod_2_2 = "different";
-var prod_2_3 = "BadgeFamilia_c";
+var prod_2_3 = "BadgeFamilia";
 var prod_3_1 = "furniture";
 var prod_3_2 = "popular";
 var prod_3_3 = "hiker";
-var prod_3_4 = "Badge_Lover_c";
-var prod_4_1 = "BadgeDoacao_c";
+var prod_3_4 = "Badge_Lover";
+var prod_4_1 = "BadgeDoacao";
 var prod_4_2 = "service";
 var prod_4_3 = "expert";
 
 
 /*QUARTO PASSO: Criar um atributo Custom chamado Pesquisa e colocar o API Name dele*/
 /*NBO*/
-var Pesquisa = "MemberEO_BadgePesquisa_c";
+var Pesquisa = "MemberEO_BadgePesquisa";
